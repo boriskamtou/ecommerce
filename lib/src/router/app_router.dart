139ba8542ed5c0aab0f6079b2fb64ad27b2f951d
Boris_K <b.kamtou@pkf-researchcenter.com>
2023-02-17 15:19:08 +0100

    setup routing

--- lib/src/router/app_router.dart
+++ lib/src/router/app_router.dart
@@ -1,4 +1,7 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/checkout/checkout_screen.dart';
+import 'package:ecommerce_app/src/features/leave_review_page/leave_review_screen.dart';
+import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
@@ -12,7 +15,9 @@ import '../features/products_list/products_list_screen.dart';
 enum AppRoute {
   home,
   product,
+  leaveReview,
   cart,
+  checkout,
   orders,
   account,
   signIn,
@@ -27,24 +32,51 @@ final goRouter = GoRouter(
       builder: (context, state) => const ProductsListScreen(),
       routes: [
         GoRoute(
-          path: 'product/:id',
-          name: AppRoute.product.name,
-          builder: (context, state) {
-            final productId = state.params['id']!;
-            return ProductScreen(productId: productId);
-          },
-        ),
+            path: 'product/:id',
+            name: AppRoute.product.name,
+            builder: (context, state) {
+              final productId = state.params['id']!;
+              return ProductScreen(productId: productId);
+            },
+            routes: [
+              GoRoute(
+                path: 'review',
+                name: AppRoute.leaveReview.name,
+                pageBuilder: (context, state) {
+                  final productId = state.params['id']!;
+                  return MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: LeaveReviewScreen(
+                      productId: productId,
+                    ),
+                  );
+                },
+              ),
+            ]),
         GoRoute(
-          path: 'cart',
-          name: AppRoute.cart.name,
-          pageBuilder: (context, state) {
-            return MaterialPage(
-              key: state.pageKey,
-              fullscreenDialog: true,
-              child: const ShoppingCartScreen(),
-            );
-          },
-        ),
+            path: 'cart',
+            name: AppRoute.cart.name,
+            pageBuilder: (context, state) {
+              return MaterialPage(
+                key: state.pageKey,
+                fullscreenDialog: true,
+                child: const ShoppingCartScreen(),
+              );
+            },
+            routes: [
+              GoRoute(
+                path: 'checkout',
+                name: AppRoute.checkout.name,
+                pageBuilder: (context, state) {
+                  return MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: const CheckoutScreen(),
+                  );
+                },
+              ),
+            ]),
         GoRoute(
           path: 'orders',
           name: AppRoute.orders.name,
@@ -83,4 +115,5 @@ final goRouter = GoRouter(
       ],
     ),
   ],
+  errorBuilder: (context, state) => const NotFoundScreen(),
 );
